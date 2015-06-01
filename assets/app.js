6c06eb082625e77ba0f94ff9ae715248763c14ee
Alex Faber <alex.faber@singlewire.com>
2015-06-01 11:22:35 -0500

    add ngResource and fetch songs (run bower install)

--- assets/app.js
+++ assets/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'ngResource', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -71,4 +71,4 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   $urlRouterProvider.otherwise('/app/playlists');
 });
 
-var mainControllers = angular.module('starter.controllers', []);
+var mainControllers = angular.module('starter.controllers', ['ngResource']);
--- assets/controllers/PlaylistCtrl.js
+++ assets/controllers/PlaylistCtrl.js
@@ -1,6 +1,20 @@
 /**
  * Created by alexfaber on 6/1/15.
  */
-mainControllers.controller('PlaylistCtrl', function($scope, $stateParams) {
+mainControllers.controller('PlaylistCtrl', function($scope, $stateParams, $resource) {
+    var songs = $resource("http://104.236.94.200:5555/challenges/demo", {limit: 10});
 
+    //Fires each time the view is revealed (Ionic Changed this recently due to their new caching system)
+    $scope.$on('$ionicView.enter', function(e) {
+
+        //alert($stateParams.playlistId);
+
+        if(!$scope.songs) {
+            songs.query().$promise.then(function (songs) {
+                $scope.songs = songs;
+            }).catch(function (error) {
+                console.error(error);
+            });
+        }
+    });
 });
